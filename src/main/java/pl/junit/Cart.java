e0d93b14dd787dbebb8b91e1c37e0f5c8c27326b
DawidDobrowolski <dawid.dobrowolski@domado.pl>
2021-02-07 00:09:48 +0100

    assertTimeout

---
+++ src/main/java/pl/junit/Cart.java
@@ -0,0 +1,28 @@
+package pl.junit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
+    private List<Order> orders = new ArrayList<>();
+
+    public void addOrderToCart(Order order){
+        this.orders.add(order);
+    }
+
+    public void clearCart(){
+        this.orders.clear();
+    }
+
+    public void simulateLargeOrder(){
+        for (int i = 0; i < 1_000; i++) {
+            Meal meal = new Meal(i%10,"Danie dnia nr. " + i);
+            Order order = new Order();
+            order.addMealToOrder(meal);
+            addOrderToCart(order);
+        }
+        System.out.println("Number of orders in cart: " + this.orders.size());
+        clearCart();
+    }
+}
